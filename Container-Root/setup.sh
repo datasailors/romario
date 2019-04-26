fcc22dbede15e93837f8c7b9ffa3d84a589ec7aa
Alex Iankoulski <iankouls@bhge.com>
2019-04-26 20:10:08 +0000

    Complete Romario Depend on Docker Automation

--- Container-Root/setup.sh
+++ Container-Root/setup.sh
@@ -6,4 +6,13 @@ if [ -d /etc/apt ]; then
         [ -f /etc/apt/apt.conf ] && cat /etc/apt/apt.conf
 fi
 
+apt-get install -y curl
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.14.0/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+
+mv ./kubectl /usr/local/bin/kubectl
+
 
